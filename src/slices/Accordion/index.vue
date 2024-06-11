38cac21f9eb7517a3da8a38ff70823b6db741932
Mateo Mateus <mateo.mateusm@gmail.com>
2024-06-10 23:52:58 -0500

    remove wrong import statement

--- src/slices/Accordion/index.vue
+++ src/slices/Accordion/index.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
-import { Content } from "@prismicio/client";
-
 // The array passed to `getSliceComponentProps` is purely optional.
 // Consider it as a visual hint for you when templating your slice.
 defineProps(
-  getSliceComponentProps<Content.AccordionSlice>([
+  getSliceComponentProps([
     "slice",
     "index",
     "slices",
--- src/slices/LogosCarousel/index.vue
+++ src/slices/LogosCarousel/index.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
-import { type Content } from "@prismicio/client";
 
 // The array passed to `getSliceComponentProps` is purely optional.
 // Consider it as a visual hint for you when templating your slice.
 defineProps(
-  getSliceComponentProps<Content.LogosCarouselSlice>([
+  getSliceComponentProps([
     "slice",
     "index",
     "slices",
